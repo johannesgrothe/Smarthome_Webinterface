a5bbb98155069ecfd5c3a3486cf4557e2843858f
Niklas Telama <n.telama@gmail.com>
2021-03-15 16:40:26 +0100

    #7 changed layout on Home

--- src/Home.js
+++ src/Home.js
@@ -47,6 +47,25 @@ export default class MainPage extends Component {
                     <Col>
                         <Card>
                             <Card.Title>
+                                Bridge Software version info
+                            </Card.Title>
+                            <Card.Body>
+                                <p>
+                                    software branch: {this.state.info.software_branch}<br/>
+                                    software commit: {this.state.info.software_commit}<br/>
+                                    python version: {this.state.info.python_version}<br/>
+                                    PIO version: {this.state.info.platformio_version}<br/>
+                                    pipenv version: {this.state.info.pipenv_version}<br/>
+                                    git version: {this.state.info.git_version}<br/>
+                                </p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Title>
                                 Clients
                             </Card.Title>
                             <Card.Body> Number of Clients: {this.state.info.client_count} </Card.Body>
@@ -66,25 +85,6 @@ export default class MainPage extends Component {
                                 Connectors
                             </Card.Title>
                             <Card.Body> Number of Connectors: {this.state.info.connector_count} </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Title>
-                                Bridge Software version info
-                            </Card.Title>
-                            <Card.Body>
-                                <p>
-                                    software branch: {this.state.info.software_branch}<br/>
-                                    software commit: {this.state.info.software_commit}<br/>
-                                    python version: {this.state.info.python_version}<br/>
-                                    PIO version: {this.state.info.platformio_version}<br/>
-                                    pipenv version: {this.state.info.pipenv_version}<br/>
-                                    git version: {this.state.info.git_version}<br/>
-                                </p>
-                            </Card.Body>
                         </Card>
                     </Col>
                 </Row>
